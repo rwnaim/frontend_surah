19c32bbd850df11bac12930949f53481401aa10d
Rana Wijdan Naim <45732575+rwnaim@users.noreply.github.com>
2020-08-02 05:13:56 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import 'assets/scss/style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      Hallo World!
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/LandingPages.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import moduleName from 'elements/Button'
+
+export default class Landingpages extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
